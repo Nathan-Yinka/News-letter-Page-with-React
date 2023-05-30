f7c91d30bc6b1002e30675a03ecf8da03cafbda4
Nathan-Yinka <oludarenathaniel@gmail.com>
2023-05-30 22:12:46 +0100

    done

--- src/component/Checkbox.js
+++ src/component/Checkbox.js
@@ -6,12 +6,7 @@ const Checkbox = ({items}) => {
         {items.map((item)=>{
             return(
                 <main className='mt-2 d-flex align-items-center' key={item.id}>
-                <img src={checkImg} alt='checking img'></img>
-                <input 
-                className='custom-checkbox'
-                type="checkbox" 
-                checked={item.checked} >
-                </input>
+                <img src={checkImg} alt='checking img d-block'></img>
                 <label 
                 className='ms-3'>
                     {item.text}
